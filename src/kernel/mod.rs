c0de101151aba1e228abc000b0beb0d07aaaf918
Christophe Augier <christophe.augier@gmail.com>
2015-12-05 23:05:50 +0100

    Test simple array of syscalls
    No parameters yet.

--- src/kernel/mod.rs
+++ src/kernel/mod.rs
@@ -26,8 +26,30 @@ pub fn handle_exception(sp: u32, state: &mut KernelState) -> usize {
     return next_task.stack_ptr;
 }
 
+static syscalls: [fn (&mut KernelState); 4] = [
+    task_spawn,
+    task_exit,
+    task_yield,
+    print,
+    ];
+
+pub fn task_spawn(ks: &mut KernelState) {
+}
+pub fn task_exit(ks: &mut KernelState) {
+}
+pub fn task_yield(ks: &mut KernelState) {
+}
+pub fn print(ks: &mut KernelState) {
+}
+
+
 #[no_mangle] // this function is called by asm
-pub fn handle_syscall(sp: u32, state: &mut KernelState, num: u32) -> usize {
+pub fn handle_syscall(sp: u32, state: &mut KernelState, num: usize) -> usize {
+
+    if num < syscalls.len() { // this also removes out of bounds panic code.
+        syscalls[num](state);
+    }
+
     {
         let cur_task = state.tasks.get_current_task_mut();
         cur_task.stack_ptr = sp as usize;
